9ad357bb1c67c3c402a5fd378f27074aa9473423
Ismael Isa <ismael.isa02@gmail.com>
2022-11-02 11:33:30 -0300

    consulta API, validar formularios, muestra de resultados

--- src/data/monedas.js
+++ src/data/monedas.js
@@ -3,6 +3,7 @@
     const monedas = [
       { id: 'USD', nombre: 'Dolar de Estados Unidos' },
       { id: 'MXN', nombre: 'Peso Mexicano' },
+      { id: 'ARG', nombre: 'Peso Argentino' },
       { id: 'EUR', nombre: 'Euro' },
       { id: 'GBP', nombre: 'Libra Esterlina' },
     ]
